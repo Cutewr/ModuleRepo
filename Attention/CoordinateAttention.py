23a8805ae445913499d5cdf5fb229373ff701f66
“wangrui230” <wangrui230@meituan.com>
2024-12-06 14:03:13 +0800

    坐标注意力 - 注释

--- Attention/CoordinateAttention.py
+++ Attention/CoordinateAttention.py
@@ -23,17 +23,21 @@ class CA_Block(nn.Module):
         self.F_w = nn.Conv2d(in_channels=channel // reduction, out_channels=channel, kernel_size=1, stride=1,
                              bias=False)
 
+
         self.sigmoid_h = nn.Sigmoid()
         self.sigmoid_w = nn.Sigmoid()
 
     def forward(self, x):
-        x_h = self.avg_pool_x(x).permute(0, 1, 3, 2)
-        x_w = self.avg_pool_y(x)
+        x_h = self.avg_pool_x(x).permute(0, 1, 3, 2)  # (3,64,1,32)
+        x_w = self.avg_pool_y(x)    # (3,64,1,32)
 
-        x_cat_conv_relu = self.relu(self.conv_1x1(torch.cat((x_h, x_w), 3)))
+        x_cat_conv_relu = self.relu(self.conv_1x1(torch.cat((x_h, x_w), 3))) # (3,4,1,64)
 
+        # x_cat_conv_split_h : (3,4,1,32)
+        # x_cat_conv_split_w : (3,4,1,32)
         x_cat_conv_split_h, x_cat_conv_split_w = x_cat_conv_relu.split([self.h, self.w], 3)
 
+        # s_h : (3,64,32,1)    s_w : (3,64,32,1)
         s_h = self.sigmoid_h(self.F_h(x_cat_conv_split_h.permute(0, 1, 3, 2)))
         s_w = self.sigmoid_w(self.F_w(x_cat_conv_split_w))
 
@@ -43,7 +47,7 @@ class CA_Block(nn.Module):
 
 
 if __name__ == '__main__':
-    x = torch.randn(1, 16, 128, 64)  # b, c, h, w
-    ca_model = CA_Block(channel=16, h=128, w=64)
+    x = torch.randn(3, 64, 32, 32)  # b, c, h, w
+    ca_model = CA_Block(channel=64, h=32, w=32)
     y = ca_model(x)
-    print(y.shape)
+    print(y.shape)  # (3, 64, 32, 32)
